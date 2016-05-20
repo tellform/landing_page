56c9ba5f0b7407c90ac14f997ec3c1ee1e8f11b8
David Baldwynn <david.baldwin@alumni.ubc.ca>
2016-05-20 17:53:29 -0400

    updated example forms

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -7,15 +7,21 @@ angular.module('core').controller('HomeController', ['$rootScope', '$scope',
 
         $scope.forms = [
             {
-                'background-color': "#fafafa",
-                'color': '#292C2F',
+                'background-color': "#E4F8A8",
+                'color': '#6D8524',
                 'title': 'Job Application Example',
                 'link': 'https://stage.tellform.com/#!/forms/571a76b856d64f9e4ca73ca1'
             },
             {
-                'background-color': "#fafafa",
-                'color': '#292C2F',
+                'background-color': "#F2F2F2",
+                'color': '#3B6161',
                 'title': 'Contact Form Example',
+                'link': 'https://stage.tellform.com/#!/forms/57196d592601ed12074eecc0'
+            },
+            {
+                'background-color': "#FEFEFF",
+                'color': '#0F3E5E',
+                'title': 'Patient Registration Example',
                 'link': 'https://stage.tellform.com/#!/forms/57196d592601ed12074eecc0'
             }   
         ];   
--- public/modules/forms/forms.client.module.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-// Use Application configuration module to register a new module
-ApplicationConfiguration.registerModule('forms', ['ngFileUpload', 'ui.router.tabs', 'colorpicker.module', 'ui.date', 'ui.sortable', 'angular-input-stars', 'users']);
