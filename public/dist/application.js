44b8d8aa96ab86701ecabceddcc92a9e5fc430c1
David Baldwynn <david.baldwin@alumni.ubc.ca>
2016-06-13 18:19:49 -0400

    make hover link text readable

--- public/dist/application.js
+++ public/dist/application.js
@@ -46,7 +46,7 @@ angular.element(document).ready(function() {
 angular.module('NodeForm.templates', []).run(['$templateCache', function($templateCache) {
   "use strict";
   $templateCache.put("../public/modules/core/views/header.client.view.html",
-    "<section class=\"navbar navbar-fixed-top navbar-inverse\" data-ng-controller=HeaderController ng-hide=hideNav><div class=container><div class=navbar-header><button class=navbar-toggle type=button data-ng-click=toggleCollapsibleMenu()><span class=sr-only>Toggle navigation</span> <span class=icon-bar></span> <span class=icon-bar></span> <span class=icon-bar></span></button> <a href=\"/\" class=navbar-brand><i class=\"fa fa-archive\" style=\"padding-right: 0.3em\"></i>Tell<span>Form</span></a></div><nav class=\"collapse navbar-collapse\" collapse=!isCollapsed role=navigation><ul class=\"nav navbar-nav navbar-right\"><li><a href=https://admin.tellform.com/#!/signup>Sign Up</a></li><li class=divider-vertical></li><li><a href=https://admin.tellform.com/#!/signin>Sign In</a></li></ul></nav></div></section>");
+    "<section class=\"navbar navbar-fixed-top navbar-inverse\" data-ng-controller=HeaderController ng-hide=hideNav><div class=container><div class=navbar-header><button class=navbar-toggle type=button data-ng-click=toggleCollapsibleMenu()><span class=sr-only>Toggle navigation</span> <span class=icon-bar></span> <span class=icon-bar></span> <span class=icon-bar></span></button> <a href=\"/\" class=navbar-brand><i class=\"fa fa-archive\" style=\"padding-right: 0.3em\"></i>Tell<span>Form</span></a></div><nav class=\"collapse navbar-collapse\" collapse=!isCollapsed role=navigation><ul class=\"nav navbar-nav navbar-right\"><li><a href=/examples>Example Forms</a></li><li><a href=https://admin.tellform.com/#!/signup>Sign Up</a></li><li class=divider-vertical></li><li><a href=https://admin.tellform.com/#!/signin>Sign In</a></li></ul></nav></div></section>");
 }]);
 
 'use strict';
@@ -83,24 +83,35 @@ angular.module('core').controller('HeaderController', ['$rootScope', '$scope', '
 
 'use strict';
 
-
 angular.module('core').controller('HomeController', ['$rootScope', '$scope', 
 	function($rootScope, $scope) {
 		$scope = $rootScope;
 
         $scope.forms = [
             {
-                'background-color': "#fafafa",
-                'color': '#292C2F',
+                'backgroundColor': '#E4F8A8',
+                'color': '#33334C',
                 'title': 'Job Application Example',
                 'link': 'https://stage.tellform.com/#!/forms/571a76b856d64f9e4ca73ca1'
             },
             {
-                'background-color': "#fafafa",
-                'color': '#292C2F',
+                'backgroundColor': "#F2F2F2",
+                'color': '#3B6161',
                 'title': 'Contact Form Example',
                 'link': 'https://stage.tellform.com/#!/forms/57196d592601ed12074eecc0'
-            }   
+            },
+            {
+                'backgroundColor': "#FAFBFF",
+                'color': '#2D4A6C',
+                'title': 'Patient Registration Example',
+                'link': 'https://stage.tellform.com/#!/forms/573f8329d18419854559c29c'
+            },
+            {
+                'backgroundColor': '#F7F7F7',
+                'color': '#2F6B99',
+                'title': 'Event Registration Form Example',
+                'link': 'https://stage.tellform.com/#!/forms/571971c72471261f0af3683e'
+            } 
         ];   
     }
 ]);
--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,29 +1,34 @@
 'use strict';
 
-
 angular.module('core').controller('HomeController', ['$rootScope', '$scope', 
 	function($rootScope, $scope) {
 		$scope = $rootScope;
 
         $scope.forms = [
             {
-                'background-color': "#E4F8A8",
-                'color': '#6D8524',
+                'backgroundColor': '#E4F8A8',
+                'color': '#33334C',
                 'title': 'Job Application Example',
                 'link': 'https://stage.tellform.com/#!/forms/571a76b856d64f9e4ca73ca1'
             },
             {
-                'background-color': "#F2F2F2",
+                'backgroundColor': "#F2F2F2",
                 'color': '#3B6161',
                 'title': 'Contact Form Example',
                 'link': 'https://stage.tellform.com/#!/forms/57196d592601ed12074eecc0'
             },
             {
-                'background-color': "#FEFEFF",
-                'color': '#0F3E5E',
+                'backgroundColor': "#FAFBFF",
+                'color': '#2D4A6C',
                 'title': 'Patient Registration Example',
-                'link': 'https://stage.tellform.com/#!/forms/57196d592601ed12074eecc0'
-            }   
+                'link': 'https://stage.tellform.com/#!/forms/573f8329d18419854559c29c'
+            },
+            {
+                'backgroundColor': '#F7F7F7',
+                'color': '#2F6B99',
+                'title': 'Event Registration Form Example',
+                'link': 'https://stage.tellform.com/#!/forms/571971c72471261f0af3683e'
+            } 
         ];   
     }
 ]);
