87f15aa36c4e4230d17ed2f62f2ea83d0573532f
David Baldwynn <david.baldwin@alumni.ubc.ca>
2016-07-19 20:14:17 -0400

    :got mailchimp newsletter to work

--- public/dist/application.js
+++ public/dist/application.js
@@ -46,7 +46,7 @@ angular.element(document).ready(function() {
 angular.module('NodeForm.templates', []).run(['$templateCache', function($templateCache) {
   "use strict";
   $templateCache.put("../public/modules/core/views/header.client.view.html",
-    "<section class=\"navbar navbar-fixed-top navbar-inverse\" data-ng-controller=HeaderController ng-hide=hideNav><div class=container><div class=navbar-header><button class=navbar-toggle type=button data-ng-click=toggleCollapsibleMenu()><span>MENU</span></button> <a href=\"/\" class=navbar-brand><i class=\"fa fa-archive\" style=\"padding-right: 0.3em\"></i>Tell<span>Form</span></a></div><nav class=\"collapse navbar-collapse\" collapse=!isCollapsed role=navigation><ul class=\"nav navbar-nav navbar-right\"><li><a href=/examples>Example Forms</a></li><li><a href=https://admin.tellform.com/#!/signup>Sign Up</a></li><li class=divider-vertical></li><li><a href=https://admin.tellform.com/#!/signin>Sign In</a></li></ul></nav></div></section>");
+    "<div class=\"navbar navbar-inverse\" data-ng-controller=HeaderController><div class=container-fluid><div class=navbar-header><a href=\"/\" class=navbar-brand><i class=\"fa fa-archive\" style=\"padding-right: 0.3em\"></i>Tell<span>Form</span></a></div></div><div class=\"container-fluid text-center\"><div class=text-center><button class=navbar-toggle type=button data-toggle=collapse><span>MENU</span></button></div><nav class=\"collapse navbar-collapse\" role=navigation><ul class=\"nav navbar-nav\"><li><a href=\"/\">Home</a></li><li><a href=/examples>Examples</a></li><li><a href=/faq>FAQ</a></li><li><a href=/about>About</a></li><li><a href=mailto:polydaic@gmail.com>Contact</a></li></ul></nav></div></div>");
 }]);
 
 'use strict';
@@ -88,7 +88,7 @@ angular.module('core').controller('HomeController', ['$rootScope', '$scope',
 		$scope = $rootScope;
 
         $scope.forms = [
-            {
+          {
                 'backgroundColor': '#ff',
                 'color': '#000',
                 'title': 'Newsletter Signup Example',
@@ -111,7 +111,7 @@ angular.module('core').controller('HomeController', ['$rootScope', '$scope',
                 'color': '#000',
                 'title': 'Market Research Survey Example',
                 'link': 'https://example.tellform.com/#!/forms/5765b23fa7fc443c354388d2'
-            } 
+            }
         ];   
     }
 ]);
